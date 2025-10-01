0fca055a46dfdc9706e3d751d3a3136e68c9457a
Olivier Halligon <olivier.halligon@automattic.com>
2025-10-01 02:13:49 +0200

    Fix notarize env var name

--- .buildkite/commands/setup_macos_code_signing.sh
+++ .buildkite/commands/setup_macos_code_signing.sh
@@ -9,6 +9,8 @@ echo "--- :key: Set up signing"
 bundle exec fastlane setup_code_signing
 
 # For `electron-builder` to find those as `notarize` options
+# See https://github.com/electron-userland/electron-builder/blob/master/packages/app-builder-lib/src/macPackager.ts#L599-L602
+# See https://www.electron.build/mac#notarize
 export APPLE_API_KEY="$APP_STORE_CONNECT_API_KEY_KEY"
 export APPLE_API_KEY_ID="$APP_STORE_CONNECT_API_KEY_KEY_ID"
-export APPLE_API_KEY_ISSUER="$APP_STORE_CONNECT_API_KEY_ISSUER_ID"
+export APPLE_API_ISSUER="$APP_STORE_CONNECT_API_KEY_ISSUER_ID"
