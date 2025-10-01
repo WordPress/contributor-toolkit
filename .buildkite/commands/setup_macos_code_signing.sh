17274e450793c83cac0842c58bb55485168617c9
Olivier Halligon <olivier.halligon@automattic.com>
2025-10-01 02:01:55 +0200

    Attempt to make prov profile + notarize work

--- .buildkite/commands/setup_macos_code_signing.sh
+++ .buildkite/commands/setup_macos_code_signing.sh
@@ -7,3 +7,8 @@ install_gems
 
 echo "--- :key: Set up signing"
 bundle exec fastlane setup_code_signing
+
+# For `electron-builder` to find those as `notarize` options
+export APPLE_API_KEY="$APP_STORE_CONNECT_API_KEY_KEY"
+export APPLE_API_KEY_ID="$APP_STORE_CONNECT_API_KEY_KEY_ID"
+export APPLE_API_KEY_ISSUER="$APP_STORE_CONNECT_API_KEY_ISSUER_ID"
