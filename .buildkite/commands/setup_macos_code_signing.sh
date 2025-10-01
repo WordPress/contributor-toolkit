f8f5fd08cfcf89fae5eae600f3da75cd3e668153
Olivier Halligon <olivier.halligon@automattic.com>
2025-10-01 03:04:28 +0200

    Cleanup

--- .buildkite/commands/setup_macos_code_signing.sh
+++ .buildkite/commands/setup_macos_code_signing.sh
@@ -8,6 +8,7 @@ install_gems
 echo "--- :key: Set up signing"
 bundle exec fastlane setup_code_signing
 
+echo "--- :electron: Expose signing config to electron-builder"
 # Export necessary env vars for `electron-builder` to find those for its `notarize` option
 # See https://github.com/electron-userland/electron-builder/blob/master/packages/app-builder-lib/src/macPackager.ts#L599-L602
 # See https://www.electron.build/mac#notarize
