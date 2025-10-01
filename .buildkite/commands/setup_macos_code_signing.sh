b4e54cf47139b5e229c9a9f0001e392754a0478d
Olivier Halligon <olivier.halligon@automattic.com>
2025-10-01 02:52:02 +0200

    Fix notarize: use key path, not content

--- .buildkite/commands/setup_macos_code_signing.sh
+++ .buildkite/commands/setup_macos_code_signing.sh
@@ -8,9 +8,10 @@ install_gems
 echo "--- :key: Set up signing"
 bundle exec fastlane setup_code_signing
 
-# For `electron-builder` to find those as `notarize` options
+# Export necessary env vars for `electron-builder` to find those for its `notarize` option
 # See https://github.com/electron-userland/electron-builder/blob/master/packages/app-builder-lib/src/macPackager.ts#L599-L602
 # See https://www.electron.build/mac#notarize
-export APPLE_API_KEY="$APP_STORE_CONNECT_API_KEY_KEY"
+echo "$APP_STORE_CONNECT_API_KEY_KEY" >.apple_api_key
+export APPLE_API_KEY=".apple_api_key"
 export APPLE_API_KEY_ID="$APP_STORE_CONNECT_API_KEY_KEY_ID"
 export APPLE_API_ISSUER="$APP_STORE_CONNECT_API_KEY_ISSUER_ID"
