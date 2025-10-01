521b217ee2e05dfc96f1b0909a248adabe5537e9
Olivier Halligon <olivier.halligon@automattic.com>
2025-10-01 10:39:48 +0200

    Use gitignore'd `.codesigning/` dir for CS secrets
    Makes it easier to `.gitignore` at once and to have all the code-signing related secret files in a single place (especially the `apple_api_key` and `*.provisionprofile` files)

--- .buildkite/commands/setup_macos_code_signing.sh
+++ .buildkite/commands/setup_macos_code_signing.sh
@@ -12,7 +12,8 @@ echo "--- :electron: Expose signing config to electron-builder"
 # Export necessary env vars for `electron-builder` to find those for its `notarize` option
 # See https://github.com/electron-userland/electron-builder/blob/master/packages/app-builder-lib/src/macPackager.ts#L599-L602
 # See https://www.electron.build/mac#notarize
-echo "$APP_STORE_CONNECT_API_KEY_KEY" >.apple_api_key
-export APPLE_API_KEY=".apple_api_key"
+mkdir -p .codesigning/
+echo "$APP_STORE_CONNECT_API_KEY_KEY" >.codesigning/apple_api_key
+export APPLE_API_KEY=".codesigning/apple_api_key"
 export APPLE_API_KEY_ID="$APP_STORE_CONNECT_API_KEY_KEY_ID"
 export APPLE_API_ISSUER="$APP_STORE_CONNECT_API_KEY_ISSUER_ID"
